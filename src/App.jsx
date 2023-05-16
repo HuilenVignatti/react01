9180001cd25de7cddd7bfc3d7ee8ef8434d261ed
hvignatti <hvignatti.developer@gmail.com>
2023-05-16 10:07:57 -0300

    primer github de react

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { Tarjeta } from "./tarjeta";
+
+function App() {
+  return <h1>estoy en app</h1>;
+}
+
+export default App;
---
+++ src/tarjeta.jsx
@@ -0,0 +1,12 @@
+export function Tarjeta() {
+  let texto = "holi";
+  let edad = 20;
+
+  return (
+    <div>
+      <img src="" alt="" />
+      <h3> {texto}</h3>
+      <h3>{edad}</h3>
+    </div>
+  );
+}
